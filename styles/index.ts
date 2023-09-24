cac327e0435cd126ba631ae4c8ad4b674acdba54
Emmanuel Akinroye <52328102+Emma66886@users.noreply.github.com>
2023-09-24 10:56:04 +0100

    Update index.ts

--- styles/index.ts
+++ styles/index.ts
@@ -3,8 +3,8 @@ import { extendTheme } from '@chakra-ui/react'
 // 2. Extend the theme to include custom colors, fonts, etc
 const colors = {
     linearGrad :'#B5179E,#7209B7',
-    100:'#B5179E',
-    200:'#7209B7'
+    "100":'#B5179E',
+    "200":'#7209B7'
 }
 const config = {
     initialColorMode: 'dark',
