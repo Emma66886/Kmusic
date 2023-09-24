7c967e0ee4d493fc1e9981bf65f8cc944ad3880f
Emmanuel Akinroye <52328102+Emma66886@users.noreply.github.com>
2023-09-24 15:38:21 +0100

    Update loadMusics.tsx

--- components/loadMusics.tsx
+++ components/loadMusics.tsx
@@ -3,51 +3,42 @@ import { Box, Flex, Input, Text } from '@chakra-ui/react'
 import Image from 'next/image'
 import React, { useEffect, useRef, useState } from 'react'
 import { dummyGenre } from '@/utils/dummyGenre'
-import { useAudioPlayer, useGlobalAudioPlayer } from 'react-use-audio-player'
-import { pathToFileURL } from 'url'
 import MusicList,{ MusicItemListType } from './musicList'
+import { readFile } from 'fs'
 function LoadMusics() {
   type returnedData = {musicData:string,trackName:string,duration?:string | string}
     const {image,link,name,trackNumber} = dummyGenre[0]
-    const [musics,setMusics] = useState<FileList>()
     const [parsedMusics,setparsedMusics] = useState<returnedData[]>()
-    
     const ref = useRef<HTMLInputElement>(null)
-    const {load,play,duration} = useAudioPlayer()
-    useEffect(()=>{
-        console.log({musics})
+    const loadMusics = async(musics:FileList)=>{
         if(musics && musics.length > 0){
             console.log("Loading musics")
-           let processsedMusics:returnedData[] = [];
+           
           //  console.log(musics.length)
-          Array.prototype.forEach.call(musics,(file)=>{
-            const reader = new FileReader();
-            let returnValue: returnedData;
-            reader.onload = (e) => {
-              let musicData = e.target?.result as string
-              let trackName = file?.name
-              processsedMusics.push({musicData,trackName})
-            };
-            reader.readAsDataURL(file);
-            console.log({file}) 
+          
+          let processsedMusics:any = Array.prototype.map.call(musics,async(file)=>{
+            try{
+            const readFile =  new Promise((resolve,reject)=>{
+              const reader = new FileReader();
+              reader.onload = (e) => {
+               let musicData = e.target?.result as string
+               let trackName = file?.name
+               resolve({musicData,trackName})
+             };
+              reader.readAsDataURL(file);
+            })
+            console.log({readFile})
+            return readFile
+          }catch(e){return {}}
+            // console.log({file}) 
           })
-          // const item = processsedMusics.map(({musicData,...args},i)=>{
-          //    load(musicData)
-          //    console.log({musicData})
-          //     return {
-          //       musicData,
-          //       duration :duration as any,
-          //       ...args
-          //     }
-          // })
-          setparsedMusics(processsedMusics)
+          const totalMusics = await Promise.all(processsedMusics) as any
+          // Array.prototype.forEach.call()
+          console.log({totalMusics})
+          setparsedMusics( totalMusics)
         }
 
-    },[musics])
-
-    useEffect(()=>{
-      console.log(parsedMusics)
-    },[parsedMusics])
+    }
   return (
     <>
     <Box mb='5' borderRadius='20px' h='200px' w='200px'  position='relative'>
@@ -56,13 +47,14 @@ function LoadMusics() {
     justifyContent='space-between' alignItems='center' backdropFilter={'blur(10px)'} p='5px 10px'>
         <Flex flexDir='column'>
         <Text>LOCAL MUSICS</Text>
-        <Text color='#DEDEDE' fontSize='0.8em'>{parsedMusics && parsedMusics?.length} Tracks picked</Text>
+        {(parsedMusics && parsedMusics?.length > 0) ?<Text color='#DEDEDE' fontSize='0.8em'>{parsedMusics && parsedMusics?.length} Tracks picked</Text>:
+        <Text color='#DEDEDE' fontSize='0.8em'>Select Musics</Text>}
         </Flex>
     <Box position='relative' h='50px' w='50px'>
         <Image src={image} alt='' fill/>
     </Box>
     </Flex>
-    <Input onChange={e=>setMusics(e.target.files as FileList)} accept='.mp3' ref={ref} h='100%' w='100%' type='file' multiple opacity='0' position='absolute' inset={0} zIndex='1'/>
+    <Input onChange={e=>loadMusics(e.target.files as FileList)} accept='.mp3' ref={ref} h='100%' w='100%' type='file' multiple opacity='0' position='absolute' inset={0} zIndex='1'/>
     </Box>
     <MusicList isDashboard={false} musicList={parsedMusics} musicListHeight='55vh'/>
     </>
