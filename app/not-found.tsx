e7d05132237ca02a9dfeca5eb1fc068144205de0
Emmanuel Akinroye <52328102+Emma66886@users.noreply.github.com>
2023-10-01 22:01:44 +0100

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 function NotFound() {
   const [isLargerThan500px] = useMediaQuery('(min-width:500px)')
   return (
-    <Box w={['100%','100%','100%','60vw']} pt='5'>
+    <Box w={['100%','100%','100%','60vw']} h='100vh' pt='5'>
         <Text fontSize='2.5em' fontWeight='800' textAlign='center'>PAGE UNDER CONSTRUCTION</Text>
         {!isLargerThan500px && <Link href='/' style={{textAlign:'center',width:'100%'}}>Click here to visit Dashboard</Link>}
     </Box>
